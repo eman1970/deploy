73d2b2b6db61bb7527713f3f0eb1ad492205e687
eman1970 <tommy.hallqvist@gmail.com>
2021-03-20 00:09:19 +0100

    2nd commit

--- src/shared/api/BackendAPI.ts
+++ src/shared/api/BackendAPI.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const DEVELOPMENT = 'http://localhost:3002'
-const PRODUCTION = 'http://localhost:3002'
+const PRODUCTION = 'https://tommytestapp-2021.herokuapp.com/'
 
 const BackendAPI = Axios.create({
     baseURL: PRODUCTION,
